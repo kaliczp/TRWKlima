5f3902dbded223226fe2351fd7e299167ae3a297
Péter Kalicz <kaliczp@gmail.com>
2021-03-26 09:40:26 +0100

    Báta meteorológiai adatok beolvasása

--- MeteoBeolv.R
+++ MeteoBeolv.R
@@ -28,3 +28,29 @@ Kolked.df <- data.frame(year = trunc(as.numeric(time(Kolked_T.ts))),
 
 ## Kölked export
 write.table(Kolked.df, "KölkedClim.txt", sep = "\t", row.names = FALSE)
+
+## Báta hőmérséklet (T)
+tti <- 2
+ttactual <- read_excel(ttfile, sheet = ttsheets[tti])
+ttactual.df <- as.data.frame(ttactual[-(1:23),-1])
+Bata_T <- round(as.numeric(t(ttactual.df[-1,-1])), 2)
+Bata_T.ts <- ts(Bata_T, c(1901,1), freq = 12)
+plot(Bata_T.ts)
+
+## Báta csapadék (P)
+tti <- 4
+ttactual <- read_excel(ttfile, sheet = ttsheets[tti])
+ttactual.df <- as.data.frame(ttactual[-(1:23),-1])
+Bata_P <- round(as.numeric(t(ttactual.df[-1,-1])), 2)
+Bata_P.ts <- ts(Bata_P, c(1901,1), freq = 12)
+plot(Bata_P.ts, typ="h")
+
+## Báta data.frame
+Bata.df <- data.frame(year = trunc(as.numeric(time(Bata_T.ts))),
+                        month = as.numeric(cycle(Bata_T.ts)),
+                        temp = Bata_T,
+                        prec = Bata_P
+                        )
+
+## Báta export
+write.table(Bata.df, "BátaClim.txt", sep = "\t", row.names = FALSE)
